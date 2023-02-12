4876d283129a07aff0a9cbab2d2e68a0bee6dfe6
swebdev100 <boxdom@gmail.com>
2023-02-12 19:43:44 +0400

    Add files via upload

---
+++ 001/lamborghini/js/main.js
@@ -0,0 +1,1 @@
+const menuBtn=document.querySelector(".menu-btn"),menuMobile=document.querySelector(".menu-mobile");menuBtn.addEventListener("click",(()=>{menuMobile.classList.toggle("menu--open")}));const swiper=new Swiper(".swiper",{loop:!0,navigation:{nextEl:".swiper-button-right",prevEl:".swiper-button-left"},breakpoints:{0:{slidesPerView:1},768:{slidesPerView:2,spaceBetween:20}}});
